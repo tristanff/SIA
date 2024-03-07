808b2dff8f440e609c011dffbca5df805efdf258
tristanff <tristan.flechard@hotmail.fr>
2024-03-07 11:51:03 -0300

    First question done

--- TP0/main.py
+++ TP0/main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from catching import attempt_catch
+from pokemon import PokemonFactory, StatusEffect
+import json
+import random
+import matplotlib.pyplot as plt
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+if __name__ == "__main__":
+    factory = PokemonFactory("pokemon.json")
+    snorlax = factory.create("snorlax", 100, StatusEffect.NONE, 1)
+    print("No noise: ", attempt_catch(snorlax, "heavyball"))
+    for _ in range(10):
+        print("Noisy: ", attempt_catch(snorlax, "heavyball", 0.15))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    factory = PokemonFactory("pokemon.json")
+    ball_type = "heavyball"
+    num_attempts = 100
+    noise_level = 0.15
+
+    pokemonName_list = ["snorlax", "jolteon", "caterpie", "onix", "mewtwo"]
+    # The tab that contains the pokemon objects
+    pokemons = []
+
+    for pokemon_name in pokemonName_list:
+        pokemon = factory.create(pokemon_name, 100, StatusEffect.NONE, 1)
+        pokemons.append(pokemon)
+
+    success_rates = []
+    for pokemon in pokemons:
+        success_count = 0
+        for _ in range(num_attempts):
+            attempt_result, capture_rate = attempt_catch(pokemon, ball_type, noise_level)
+            if attempt_result:
+                success_count += 1
+        success_rate = success_count / num_attempts
+        success_rates.append(success_rate)
+
+    plt.bar(pokemonName_list, success_rates)
+    plt.xlabel('Pokémon')
+    plt.ylabel('Success Rate')
+    plt.title('Success Rate of Catching Pokémon')
+    plt.show()
